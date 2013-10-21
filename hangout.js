d90958b8a56003e250c620985b56a3c088a7fbd0
Eli Perelman <eli@eliperelman.com>
2013-10-21 10:33:26 -0500

    Initial commit

---
+++ hangout.js
@@ -0,0 +1,96 @@
+// Description
+//   Generate a Google Hangout Link
+//
+// Dependencies:
+//   "googleclientlogin": "",
+//   "moment": ""
+//
+// Commands:
+//   hubot hangout - create a link to a newly-generated Google Hangout
+//
+// Author:
+//   eliperelman
+
+var moment = require('moment');
+var googleapis = require('./hangout-core/googleapis');
+var GoogleClientLogin = require('googleclientlogin').GoogleClientLogin;
+var OAuth2Client = googleapis.OAuth2Client;
+
+var getHangoutLink = function (callback) {
+	var googleAuth = new GoogleClientLogin({
+		email: 'e@eliperelman.com',
+		password: 'appendToHangout',
+		service: 'calendar',
+		accountType: GoogleClientLogin.accountTypes.hostedOrGoogle
+	});
+
+	googleAuth.on(GoogleClientLogin.events.login, function () {
+
+		googleapis.load('calendar', 'v3', function (err, client) {
+			var token = googleAuth.getAuthId();
+
+			var oauthClient = new OAuth2Client('', '', '', {}, {
+				access_token: token
+			}, 'GoogleLogin auth=' + token);
+
+			var now = moment().format();
+
+			client.withApiKey('AIzaSyD3shHHW4YautPlu_N2KKo6Zoe9LqyE7ww');
+
+			client
+				.calendar
+				.events
+				.insert({
+					calendarId: 'primary',
+					resource: {
+						summary: 'hangout',
+						description: 'hangout',
+						reminders: {
+							overrides: {
+								method: 'popup',
+								minutes: 0
+							}
+						},
+						start: {
+							dateTime: now
+						},
+						end: {
+							dateTime: now
+						},
+						attendees: [{
+							email: 'e@eliperelman.com'
+						}]
+					}
+				})
+				.withAuthClient(oauthClient)
+				.execute(function (err, event) {
+					if (err) {
+						callback(err, null);
+					} else {
+						callback(null, event.hangoutLink);
+					}
+				});
+		});
+	});
+
+	googleAuth.on(GoogleClientLogin.events.error, function (e) {
+		callback(e, null);
+	});
+
+	googleAuth.login();
+};
+
+module.exports = function (hubot) {
+
+	hubot.respond(/hangout/i, function (msg) {
+		getHangoutLink(function (err, link) {
+			if (err) {
+				hubot.reply('An error occurred while trying to create the Google Hangout. :/');
+				return;
+			}
+
+			hubot.send('Hangout started: ' + link);
+		});
+	});
+
+};
